4a38ae9d84e681f28524ec8020853f5306ff4006
mguedesdev <mathheus.gr@gmail.com>
2023-09-06 15:02:59 -0300

    Change page without router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import './App.css'
 import Navbar_component from './components/navbar/navbar'
 import About from './pages/about'
+import Notes from './pages/notes';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
 
 
 function App() {
-
+  const [currentPage, setCurrentPage] = useState(1);
+  useEffect(() => {
+    console.log(currentPage);
+  }, [currentPage])
   return (
     <>
-      <Navbar_component/>
-      <About></About>
+      <Navbar_component setCurrentPage={setCurrentPage}/>
+      {currentPage === 1 ? <About/> : null}
+      {currentPage === 2 ? <Notes/> : null}
+      {currentPage === 3 ? <About/> : null}
+      {currentPage === 4 ? <About/> : null}
     </>
   )
 }
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,15 +3,15 @@ import { Navbar, Nav } from 'react-bootstrap'
 import "./Navbar.css"
 
 
-function Navbar_component() {
+function Navbar_component(props) {
   return (
       <Navbar bg='dark' variant='dark' >
         <Navbar.Brand className="mx-3" >Web Sr.Batata</Navbar.Brand>
         <Nav>
-          <Nav.Link>Sobre</Nav.Link>
-          <Nav.Link>Anotações</Nav.Link>
-          <Nav.Link>Blog</Nav.Link>
-          <Nav.Link>Notícias</Nav.Link>
+          <Nav.Link onClick={() => {props.setCurrentPage(1)}}>Sobre</Nav.Link>
+          <Nav.Link onClick={() => {props.setCurrentPage(2)}}>Anotações</Nav.Link>
+          <Nav.Link onClick={() => {props.setCurrentPage(3)}}>Blog</Nav.Link>
+          <Nav.Link onClick={() => {props.setCurrentPage(4)}}>Notícias</Nav.Link>
         </Nav>
       </Navbar>
   )
