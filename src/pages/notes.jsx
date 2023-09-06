5d1e3136d9081f5ce5846d73772db85716be213d
mguedesdev <mathheus.gr@gmail.com>
2023-09-06 17:02:26 -0300

    Modal pronto

--- src/pages/notes.jsx
+++ src/pages/notes.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { Container } from 'react-bootstrap';
+
 
 function Notes() {
   return (
-    <div>notes</div>
+    <>
+        <Container className="p-3 my-3 bg-light border">
+            <h1>Minhas Anotações</h1>
+            <p>
+                Use essa janela adicionar anotações, lembretes ou qualquer tipo de informação
+            </p>
+        </Container>
+    </>
   )
 }
 
